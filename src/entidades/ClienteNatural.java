5bd394cbcbe8967c06ee399f3443f695348b1de0
yolver50 <aguilarmezayonel50@gmail.com>
2024-12-05 11:05:54 -0500

    cambio1(modificacion y agregacion de clases en registro de clientes)

---
+++ src/entidades/ClienteNatural.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package entidades;
+
+/**
+ *
+ * @author Asus
+ */
+public class ClienteNatural extends Cliente {
+    private String identificacion;
+    private String fechaNacimiento;
+    private String estadoCivil;
+
+    public ClienteNatural(String identificacion, String fechaNacimiento, String estadoCivil, String telefono, String correoElectronico, String direccion, String nombre, String apellido, int tipoCliente) {
+        super(telefono, correoElectronico, direccion, nombre, apellido, tipoCliente);
+        this.identificacion = identificacion;
+        this.fechaNacimiento = fechaNacimiento;
+        this.estadoCivil = estadoCivil;
+    } 
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+
+    public void setFechaNacimiento(String fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+
+    public String getEstadoCivil() {
+        return estadoCivil;
+    }
+
+    public void setEstadoCivil(String estadoCivil) {
+        this.estadoCivil = estadoCivil;
+    }
+   
+
+    @Override
+    public String toString() {
+        return  super.toString() + "\n" +
+                //"Apellidos: " + getApellido() + "\n" +
+                "Identificacion: " + getIdentificacion() + "\n" +
+                "Fecha de nacimiento: " + getFechaNacimiento() + "\n" +
+                "Estado civil: " +  getEstadoCivil() + "\n" +
+                "Telefono: " + getTelefono() + "\n" +
+                "Correo electronico: " + getCorreoElectronico() + "\n" +
+                "Direccion de residencia: " +  getDireccion() + "\n";
+    } 
+    
+}
--- src/entidades/Persona.java
+++ src/entidades/Persona.java
@@ -10,35 +10,31 @@ package entidades;
  */
 public abstract class Persona {
     private String nombre;
-    private String apellido;
-
-    public Persona() {
-        this("NN", "NA");
-    }
-
+  private String apellido;
+    
     public Persona(String nombre, String apellido) {
         this.nombre = nombre;
         this.apellido = apellido;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
     }
 
     public String getNombre() {
         return nombre;
     }
 
-    public void setApellido(String apellido) {
-        this.apellido = apellido;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
     }
 
     public String getApellido() {
         return apellido;
     }
 
-    @Override
-    public String toString() {
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+  @Override
+  public String toString() {
         return getNombre() + " " + getApellido();
     }
 }
